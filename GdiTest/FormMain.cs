a46dade18129c7bf476691c1611b41da7b12993f
sim511777 <sim51177@gmail.com>
2022-07-26 11:20:15 +0900

    초기 커밋.

---
+++ GdiTest/FormMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GdiTest {
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+            Console.SetOut(new TextBoxWriter(tbxLog));
+            Console.WriteLine("TextBox console generated");
+        }
+    }
+}
