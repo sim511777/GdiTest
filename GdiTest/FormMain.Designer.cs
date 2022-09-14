1f94fd1eb9a96f1a19c452706e6468e1ae2c5e30
sim511777 <sim51177@gmail.com>
2022-09-14 14:20:20 +0900

    PictureBox 그리기

--- GdiTest/FormMain.Designer.cs
+++ GdiTest/FormMain.Designer.cs
@@ -23,20 +23,37 @@
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent() {
+            this.pbx = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbx
+            // 
+            this.pbx.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbx.Location = new System.Drawing.Point(0, 0);
+            this.pbx.Name = "pbx";
+            this.pbx.Size = new System.Drawing.Size(800, 450);
+            this.pbx.TabIndex = 0;
+            this.pbx.TabStop = false;
+            this.pbx.Paint += new System.Windows.Forms.PaintEventHandler(this.pbx_Paint);
+            this.pbx.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbx_MouseMove);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbx);
             this.Name = "FormMain";
             this.Text = "GDI Test";
+            ((System.ComponentModel.ISupportInitialize)(this.pbx)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbx;
     }
 }
 
--- GdiTest/FormMain.cs
+++ GdiTest/FormMain.cs
@@ -10,8 +10,24 @@ using System.Windows.Forms;
 
 namespace GdiTest {
     public partial class FormMain : Form {
+        private Point pt;
+
         public FormMain() {
             InitializeComponent();
+        }
+
+        private void pbx_MouseMove(object sender, MouseEventArgs e) {
+            if (!e.Button.HasFlag(MouseButtons.Left))
+                return;
+            pt = e.Location;
+            pbx.Invalidate();
+        }
+
+        private void pbx_Paint(object sender, PaintEventArgs e) {
+            var br = Brushes.Red;
+            var rect = new Rectangle(pt.X - 100, pt.Y - 100, 200, 200);
+            e.Graphics.FillEllipse(br, rect);
+            Console.WriteLine($"FillEllipse({br}, {rect})");
         }
     }
 }
